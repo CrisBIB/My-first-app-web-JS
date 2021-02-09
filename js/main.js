0a6958797372912785a5291943c468b838bc3873
CCrisBIB <cristinabiglesias@gmail.com>
2021-02-09 10:38:05 +0100

    Apply funciton for searchinf and connect to API for results

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 'user strict';
 
+// Global var 
 let shows = [];
-let favoriteShows = [];
+let favorites = [];
+
+const inputElement = document.querySelector('.js-input');
+let inputValue = inputElement.value;
+
+const searchElement = document.querySelector('.js-button');
+
+// Search by click
+function handleSearch() {
+    getDataFromApi(inputElement.value)
+};
+
+searchElement.addEventListener('click', handleSearch);
+
+// Call to API
+function getDataFromApi(inputValue) {
+    fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
+        .then(response => response.json())
+        .then((data) => {
+            paintShows(data);
+            console.log(data)
+    })
+}
+
+// Paint search results
+function paintShows() {
+    let htmlCode = "";
+    for (const show of shows) {
+        console.log(data.show)
+    }
+
+}
+
+
+
