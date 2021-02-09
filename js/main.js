5c959558b0c1a719b8af2c0af6992711a455928c
CCrisBIB <cristinabiglesias@gmail.com>
2021-02-09 14:57:53 +0100

    Work in JS: add functions for callToApi, for generateHtmlCode, for paintShows and for listenEvents

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Global var 
 let shows = [];
-let favorites = [];
+let favourites = [];
 
 const inputElement = document.querySelector('.js-input');
 let inputValue = inputElement.value;
@@ -13,7 +13,6 @@ const searchElement = document.querySelector('.js-button');
 function handleSearch() {
     getDataFromApi(inputElement.value)
 };
-
 searchElement.addEventListener('click', handleSearch);
 
 // Call to API
@@ -22,18 +21,79 @@ function getDataFromApi(inputValue) {
         .then(response => response.json())
         .then((data) => {
             paintShows(data);
-            console.log(data)
-    })
-}
+        }
+     )
+};
+
+// Avoid form submit
+const formElement = document.querySelector('.js-form');
+function handleForm (ev) {
+    ev.preventDefault();
+};
+formElement.addEventListener('submit', handleForm);
 
 // Paint search results
-function paintShows() {
-    let htmlCode = "";
+const resultsElement = document.querySelector('.js-results');
+function paintShows (shows) {
+    let htmlCodeTotal = "";
     for (const show of shows) {
-        console.log(data.show)
-    }
+        htmlCodeTotal += getShowsHtmlCode(show);
+        console.log(show);
+    };
+    resultsElement.innerHTML = `<ul>${htmlCodeTotal}</ul>`;
+    listenClickForFav();
+};
 
+// Generate html code for search results
+function getShowsHtmlCode(dataShow) {
+    let htmlCode = "";
+    let showName = dataShow.show.name;
+    let showImg = dataShow.show.image;
+    let placeholderImg = `https://via.placeholder.com/210x295/ffffff/666666/?
+        text=TV`;
+    htmlCode += `<li class="js-show show">`;
+    if (showImg !== null) {
+        htmlCode += `<img src="${showImg.medium}"></img>`;
+    } else {
+        htmlCode += `<img src="${placeholderImg}"></img>`
+        };
+    htmlCode += `<h3 class="title">${showName}</h3>`;
+    htmlCode += `</li>`;
+    return htmlCode;
+};
+
+
+//Event select as favourite in search results
+
+
+const selectFavourite = ev => {
+    console.log('entró en la func')
+    const showSelect = ev.currentTarget;
+
+    showSelect.classList.toggle('showFavourite');
+}
+
+function listenClickForFav() {
+    const resultsShow = document.querySelectorAll('.js-show');
+    for (const showSelect of resultsShow) {
+    showSelect.addEventListener('click', selectFavourite);
+    }
 }
 
 
 
+
+/*
+// Paint favouries list
+function paintFavourites() {
+    getFavouritesHtmlCode();
+};
+
+function addShowToFavourites (id) {
+    let FavouriteShow = 
+    favorites.push({
+        id: show.show.id;
+        name: show.show.name;
+        image: show.show.image.medium;
+    })
+} */
