4a05df24a66239532bc3d3c289b481839e5d9263
CCrisBIB <cristinabiglesias@gmail.com>
2021-02-10 10:55:28 +0100

    Add JS rest of functions, except the one to remove foavrites and LS

--- js/main.js
+++ js/main.js
@@ -1,99 +1,133 @@
 'user strict';
 
-// Global var 
+// Global var
 let shows = [];
-let favourites = [];
+let favorites = [];
 
 const inputElement = document.querySelector('.js-input');
-let inputValue = inputElement.value;
 
-const searchElement = document.querySelector('.js-button');
-
-// Search by click
-function handleSearch() {
-    getDataFromApi(inputElement.value)
-};
-searchElement.addEventListener('click', handleSearch);
-
-// Call to API
-function getDataFromApi(inputValue) {
-    fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
-        .then(response => response.json())
-        .then((data) => {
-            paintShows(data);
-        }
-     )
-};
+console.log(inputElement.value);
 
 // Avoid form submit
 const formElement = document.querySelector('.js-form');
-function handleForm (ev) {
+function handleForm(ev) {
     ev.preventDefault();
 };
 formElement.addEventListener('submit', handleForm);
 
-// Paint search results
-const resultsElement = document.querySelector('.js-results');
-function paintShows (shows) {
-    let htmlCodeTotal = "";
-    for (const show of shows) {
-        htmlCodeTotal += getShowsHtmlCode(show);
-        console.log(show);
-    };
-    resultsElement.innerHTML = `<ul>${htmlCodeTotal}</ul>`;
-    listenClickForFav();
+// Search by click
+const searchElement = document.querySelector('.js-button');
+function handleSearch() {
+    getDataFromApi();
 };
 
-// Generate html code for search results
-function getShowsHtmlCode(dataShow) {
+searchElement.addEventListener('click', handleSearch);
+
+// Call to API
+function getDataFromApi() {
+    let inputValue = inputElement.value;
+    fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
+        .then(response => response.json())
+        .then((data) => {
+            shows = data;
+            paintShowsInResults();
+        }
+    )
+};
+
+// Paint search results
+const resultsElement = document.querySelector('.js-results')
+const paintShowsInResults = () => {
+    let htmlCodeTotal = "";
+    for (let show of shows) {
+        htmlCodeTotal += getShowsHtmlCode(show);
+    };
+    resultsElement.innerHTML = `<ul>${htmlCodeTotal}</ul>`;
+    listenShowsEvents();
+    
+};
+
+function getShowsHtmlCode(show) {
     let htmlCode = "";
-    let showName = dataShow.show.name;
-    let showImg = dataShow.show.image;
-    let placeholderImg = `https://via.placeholder.com/210x295/ffffff/666666/?
-        text=TV`;
-    htmlCode += `<li class="js-show show">`;
-    if (showImg !== null) {
-        htmlCode += `<img src="${showImg.medium}"></img>`;
+    let isFavoriteClass;
+    if (isFavoriteShow(show)) {
+        isFavoriteClass = 'selecFavorite';
     } else {
-        htmlCode += `<img src="${placeholderImg}"></img>`
+        isFavoriteClass = '';
+    }
+    htmlCode += `<li class="js-show show ${isFavoriteClass}" data-id=${show.show.id}>`;
+    if (show.show.image !== null) {
+        htmlCode += `<img src="${show.show.image.medium}"></img>`;
+    } else {
+        htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?
+        text=TV"></img>`
         };
-    htmlCode += `<h3 class="title">${showName}</h3>`;
+    htmlCode += `<h3 class="title">${show.show.name}</h3>`;
     htmlCode += `</li>`;
     return htmlCode;
 };
 
-
-//Event select as favourite in search results
-
-
-const selectFavourite = ev => {
-    console.log('entró en la func')
-    const showSelect = ev.currentTarget;
-
-    showSelect.classList.toggle('showFavourite');
-}
-
-function listenClickForFav() {
-    const resultsShow = document.querySelectorAll('.js-show');
-    for (const showSelect of resultsShow) {
-    showSelect.addEventListener('click', selectFavourite);
-    }
-}
-
-
-
-
-/*
 // Paint favouries list
-function paintFavourites() {
-    getFavouritesHtmlCode();
+const favoritesList = document.querySelector('.js-favorites');
+
+const getFavoritesHtmlCode = favorite => {
+    let htmlCode = "";
+    htmlCode += `<li class="favorites" id=${favorite.show.id}>`;
+    if (favorite.show.image !== null) {
+        htmlCode += `<img class="favoriteImg" src="${favorite.show.image.medium}"></img>`;
+    } else {
+        htmlCode += `<img class="favoriteImg" src="https://via.placeholder.com/210x295/ffffff/666666/?
+        text=TV"></img>`
+        };
+    htmlCode += `<h3 class="favoriteTitle">${favorite.show.name}</h3>`;
+    htmlCode += `</li>`;
+    return htmlCode;
 };
 
-function addShowToFavourites (id) {
-    let FavouriteShow = 
-    favorites.push({
-        id: show.show.id;
-        name: show.show.name;
-        image: show.show.image.medium;
-    })
-} */
+const paintShowsinFavorites = () => {
+    favoritesList.innerHTML = "";
+    let htmlCodeTotal = "";
+    for (let favorite of favorites) {
+        htmlCodeTotal += getFavoritesHtmlCode(favorite);
+    };
+    favoritesList.innerHTML += `<ul>${htmlCodeTotal}</ul>`;
+    
+    listenShowsEvents();
+};
+
+//Event to us/select as favorite from search result
+function listenShowsEvents() {
+    const showsElements = document.querySelectorAll('.js-show');
+    for (const showElement of showsElements) {
+        showElement.addEventListener('click', addToFavorites);
+    }
+};
+
+const addToFavorites = ev => {
+    // Get id from clicked show
+    const clickedShowId = parseInt(ev.currentTarget.dataset.id);
+    // Find show clicked
+    let foundShow;
+    for (const show of shows) {
+        if (show.show.id === clickedShowId) {
+            foundShow = show;
+        }
+    }
+    // Add show to favorite list
+    favorites.push(foundShow);
+    paintShowsinFavorites();
+    paintShowsInResults();
+};
+
+// Function to identify as favourite
+function isFavoriteShow(show) {
+    const favoriteFound = favorites.find((favorite) => {
+        console.log (favorite.show.id, show.show.id)
+        return favorite.show.id === show.show.id;
+    });
+    if (favoriteFound === undefined) {
+      return false;
+    } else {
+      return true;
+    }
+};
